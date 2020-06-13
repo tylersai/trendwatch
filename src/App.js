11d5403ee76c5b136062b9c659ea9c9ad9c5fa9f
Sai Han <thesaihan@gmail.com>
2020-06-13 17:51:21 +0630

    fetch success

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import React from 'react';
 import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
 
+import axios from "axios";
 import HomePage from './components/pages/HomePage';
+import { API_END_POINT } from './utils/constant';
+
+axios.defaults.baseURL = API_END_POINT;
+axios.defaults.headers.common['Authorization'] = `Bearer ${process.env.REACT_APP_ACCESS_TOKEN}`;
 
 function App() {
   return (
--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button } from "antd";
 import './HomePage.css';
 
+import axios from "axios";
+import { POSTER_PATH } from "../../utils/constant";
+
 function HomePage() {
+
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState([]);
+
+  const fetchData = async () => {
+    setLoading(true);
+    const res = await axios.get("/trending/movie/week");
+    console.log(res.data);
+    setLoading(false);
+    setData(res.data.results);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="HomePage">
       <h2>Hello World</h2>
       <Button type="primary">Click Me</Button>
+      <h3>{data.length}</h3>
     </div>
   );
 }
