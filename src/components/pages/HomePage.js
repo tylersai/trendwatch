9f4facc52ac78e162a5f7727183241983627e488
Sai Han <thesaihan@gmail.com>
2020-06-13 21:51:20 +0630

    Card design

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { Button } from "antd";
+import { Divider, List, Card } from "antd";
+import { HeartOutlined, PlayCircleOutlined, DownloadOutlined } from "@ant-design/icons";
 import './HomePage.css';
 
 import axios from "axios";
@@ -10,12 +11,33 @@ function HomePage() {
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState([]);
 
+  const gridOptions = {
+    gutter: 16,
+    xs: 1,
+    sm: 2,
+    md: 4,
+    lg: 4,
+    xl: 6,
+    xxl: 3,
+  };
+  const actionIcons = [
+    <HeartOutlined key="favourite" />,
+    <PlayCircleOutlined key="play" />,
+    <DownloadOutlined key="download" />,
+  ];
+
   const fetchData = async () => {
     setLoading(true);
-    const res = await axios.get("/trending/movie/week");
-    console.log(res.data);
-    setLoading(false);
-    setData(res.data.results);
+    try {
+      const res = await axios.get("/trending/movie/week");
+      console.log(res.data);
+      setLoading(false);
+      setData(res.data.results);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+      setData([]);
+    }   
   };
 
   useEffect(() => {
@@ -24,9 +46,24 @@ function HomePage() {
 
   return (
     <div className="HomePage Page">
-      <h2>Hello World</h2>
-      <Button type="primary">Click Me</Button>
-      <h3>{data.length}</h3>
+      <h2>Movies</h2>
+      <Divider orientation="center">Trending</Divider>
+      <List
+        loading={loading}
+        grid={gridOptions}
+        dataSource={data}
+        renderItem={item => (
+          <List.Item>
+            <Card
+              cover={<img className="poster" alt="POSTER" src={POSTER_PATH + item.poster_path} />}
+              bodyStyle={{padding: "12px 8px"}}
+              actions={actionIcons}
+              >
+                <Card.Meta title={item.title} />
+            </Card>
+          </List.Item>
+        )}
+       />
     </div>
   );
 }
