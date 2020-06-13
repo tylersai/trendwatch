8458afacccd7b3d755cf0aeacc25fa5725fc0d28
Sai Han <thesaihan@gmail.com>
2020-06-14 01:15:45 +0630

    Add header and back btn in MoviePage

--- src/components/pages/MoviePage.js
+++ src/components/pages/MoviePage.js
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import { Typography, Skeleton } from "antd";
+import { useHistory } from "react-router-dom";
+import { PageHeader, Typography, Skeleton } from "antd";
+import { LeftOutlined } from "@ant-design/icons";
 import './MoviePage.css';
 
 import axios from "axios";
 import { POSTER_PATH } from "../../utils/constant";
 
 const MoviePage = ({match}) => {
+
+  const history = useHistory();
 
   const [loading, setLoading] = useState(false);
   const [movie, setMovie] = useState({});
@@ -30,13 +34,20 @@ const MoviePage = ({match}) => {
     fetchData(match.params.id);
   }, [match.params.id]);
 
+  const goBack = () => history.goBack();
+
   return (
     <div className="MoviePage Page">
       <Skeleton loading={loading} avatar={{style:{borderRadius: "10px", width: "100px", height: "100px"}, shape:"square"}} round active>
-        
+        <PageHeader
+          className="movie-page-header"
+          title={movie.title}
+          backIcon={<LeftOutlined />}
+          onBack={goBack}
+          />
         <img className="poster" alt="POSTER" style={{maxHeight: "300px"}} src={POSTER_PATH + movie.poster_path} />
-        <br/>
-        <h2>{movie.title}</h2>
+        <br />
+        <br />
         <Paragraph>{movie.overview}</Paragraph>
       </Skeleton>
     </div>
