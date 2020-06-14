365319edeaa78639dd822fad0fdf263aada3dece
Sai Han <thesaihan@gmail.com>
2020-06-14 14:34:47 +0630

    Add Popularity component

---
+++ src/components/ui/Popularity.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Popularity.css";
+
+const Popularity = ({ children }) => {
+
+    const num = children ? parseInt(children/2) : 0;
+
+    return (
+        <div className="Popularity bg">
+            <div className="pop-wrap">
+                {
+                    [...Array(10).keys()].map((b,i) => <div key={i} className={i < num ? "bg-primary":"bg-secondary"}></div>)
+                }
+            </div>
+        </div>
+    )
+};
+
+export default Popularity;
