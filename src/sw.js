21a4cef89c12a27f2c46a7f77b86cf1de5789ca9
Sai Han <thesaihan@gmail.com>
2020-06-16 01:40:23 +0630

    Cache images and api responses

--- src/sw.js
+++ src/sw.js
@@ -1,3 +1,5 @@
+const { API_END_POINT } = require("./utils/constant");
+
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/5.1.3/workbox-sw.js");
 
 //workbox.setConfig({modulePathPrefix: "workbox-v5.1.3/"});
@@ -5,3 +7,29 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/5.1.3/workbox
 const precacheManifest = []/*TO_INJECT_HERE*/;
 
 workbox.precaching.precacheAndRoute(precacheManifest);
+
+const IMAGE_CACHE_NAME = "tw-images";
+const DATA_CACHE_NAME = "tw-data";
+
+// cache images with cache first strategy
+workbox.routing.registerRoute(
+  /\.(?:png|gif|jpg|jpeg|svg)$/,
+  new workbox.strategies.CacheFirst({
+    cacheName: IMAGE_CACHE_NAME,
+    plugins: [
+      new workbox.expiration.ExpirationPlugin({
+        maxEntries: 60, // max 60 images
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      }),
+    ],
+  }),
+);
+
+// cache response for GET trending movies or movie/:id for a single day with StaleWhileRevalidate
+workbox.routing.registerRoute(
+  ({url}) => url.pathname.startsWith(`${API_END_POINT}/trending`) || url.pathname.startsWith(`${API_END_POINT}/movie`),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: DATA_CACHE_NAME
+  }),
+  "GET"
+);
