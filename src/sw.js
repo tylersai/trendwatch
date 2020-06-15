a9a778fd79b1e0bac707f0d45885973f7d763437
Sai Han <thesaihan@gmail.com>
2020-06-16 04:30:41 +0630

    sw.js opaque response cors api

--- src/sw.js
+++ src/sw.js
@@ -1,5 +1,3 @@
-const { API_END_POINT } = require("./utils/constant");
-
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/5.1.3/workbox-sw.js");
 
 //workbox.setConfig({modulePathPrefix: "workbox-v5.1.3/"});
@@ -11,10 +9,30 @@ workbox.precaching.precacheAndRoute(precacheManifest);
 const IMAGE_CACHE_NAME = "tw-images";
 const DATA_CACHE_NAME = "tw-data";
 
+// cache response for GET trending movies for a single day with StaleWhileRevalidate
+workbox.routing.registerRoute(
+  new RegExp('https://api\\.themoviedb\\.org/3/trending/*'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: DATA_CACHE_NAME
+  }),
+  "GET"
+);
+
+// cache response for GET movie/:id with StaleWhileRevalidate
+workbox.routing.registerRoute(
+  new RegExp('https://api\\.themoviedb\\.org/3/movie/*'),
+  // ({url}) => url.pathname.startsWith('/movie'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: DATA_CACHE_NAME
+  }),
+  "GET"
+);
+
 // cache images with cache first strategy
 workbox.routing.registerRoute(
-  /\.(?:png|gif|jpg|jpeg|svg)$/,
-  new workbox.strategies.CacheFirst({
+  //'https://image.tmdb.org/t/p/w500/*.jpeg',
+  new RegExp('https://image\\.tmdb\\.org/t/p/w500/*\\.*'),
+  new workbox.strategies.StaleWhileRevalidate({
     cacheName: IMAGE_CACHE_NAME,
     plugins: [
       new workbox.expiration.ExpirationPlugin({
@@ -23,13 +41,4 @@ workbox.routing.registerRoute(
       }),
     ],
   }),
-);
-
-// cache response for GET trending movies or movie/:id for a single day with StaleWhileRevalidate
-workbox.routing.registerRoute(
-  ({url}) => url.pathname.startsWith(`${API_END_POINT}/trending`) || url.pathname.startsWith(`${API_END_POINT}/movie`),
-  new workbox.strategies.StaleWhileRevalidate({
-    cacheName: DATA_CACHE_NAME
-  }),
-  "GET"
 );
