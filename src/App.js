d00a03f58e35bc9bdcc69224ddada907232251be
Sai Han <thesaihan@gmail.com>
2020-06-17 03:04:44 +0630

    PWA install prompt

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
 
@@ -6,11 +6,51 @@ import axios from "axios";
 import { API_END_POINT } from './utils/constant';
 import HomePage from './components/pages/HomePage';
 import MoviePage from './components/pages/MoviePage';
+import { notification, Button } from "antd";
 
 axios.defaults.baseURL = API_END_POINT;
 axios.defaults.headers.common['Authorization'] = `Bearer ${process.env.REACT_APP_ACCESS_TOKEN}`;
 
 function App() {
+  const [deferredPrompt, setDeferredPrompt] = useState(null);
+
+  const key = `open${Date.now()}`;
+  const confirmInstall = () => {
+    notification.close(key);
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then(choice => {
+      if(choice.outcome === "accepted") {
+        console.log("User Aaccepted A2HS prompt.");
+      }
+    });
+  };
+  const btn = (
+    <Button type="primary" size="small" shape="round" onClick={confirmInstall}>
+      Confirm
+    </Button>
+  );
+  const notiConfig = {
+    message: 'Install Trend Watch',
+    description:
+      'Do you want to add Trend Watch to your home screen? Click confirm to install!',
+    duration: 0,
+    key,
+    btn
+  };
+
+  useEffect(() => {
+    window.addEventListener("beforeinstallprompt", (e) => {
+      e.preventDefault();
+      setDeferredPrompt(e);
+    });
+    if(process.env.NODE_ENV === 'production') {
+      setTimeout(() => {
+        console.log(deferredPrompt);
+        deferredPrompt && notification.info(notiConfig);
+      }, 5000);
+    }
+  }, [deferredPrompt, notiConfig]);
+
   return (
     <div className="App">
       <Router>
